735ffb6dba7dce45f439ae7bd7b84a94c3e685a8
rhpvvk <120340083+rhpvvk@users.noreply.github.com>
2023-07-22 10:13:03 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-importScripts("sw-toolbox.js"); toolbox.precache(["index.html","style.css","todo.js"]); toolbox.router.get("/images/*", toolbox.cacheFirst); toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5});
